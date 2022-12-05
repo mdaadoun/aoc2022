7002ddadaba915721612fc915b28d78a30834a4e
mdaadoun <daadounmichael@gmail.com>
2022-12-05 17:05:11 +0100

    day05

--- day05/main.py
+++ day05/main.py
@@ -8,58 +8,60 @@ move 3 from 1 to 3
 move 2 from 2 to 1
 move 1 from 1 to 2"""
 
+stacks = []
 crates = []
 
 # set crates array and return stacks number
 def setcrates(input):
-    global crates
-    crates = [['']*20 for i in range(50)]
-    lines = 0
-    stacks = 0
-    c = 25
+    global  crates
     for i in input:
-        for letter in i:
-            if letter != '[' and letter != ']':
-                crates[c].append(letter)
-        if len(crates[c]) == 0:
+        arr = []
+        if (len(i) == 0):
             break
-        c += 1
-        lines += 1
-    for i in crates[lines - 1]:
-        if i != ' ':
-            stacks += 1
-    return stacks
+        for c, j in enumerate(i):
+            if (c % 2):
+                arr.append(j)
+        crates.append(arr)
+    crates.pop()
+    crates = crates[::-1]
+    for i in range(len(crates)):
+        stacks.append([])
+    x = 0
+    for i in range(len(crates)):
+        for j in range(len(crates)):
+            stacks[i].append(crates[j][x])
+        x += 2
 
 def printcrates():
-    for c in crates:
+    for c in stacks:
         if len(c) != 0:
             print(c)
 
+
+def cleanstacks():
+    for i, v in enumerate(stacks):
+        for x in range(len(v), 0, -1):
+            if stacks[i][x - 1] == ' ':
+                stacks[i].pop()
+
 def part1(input):
     print("part 1:")
-    stacks = setcrates(input)
-    print(crates)
-    for i in input:
-        cmd = i.split(' ')
-        if cmd[0] == "move":
-            nb_move = int(cmd[1])
-            nb_from = int(cmd[3]) - 1
-            nb_to   = int(cmd[5]) - 1
-            for i in range(0, nb_move):
-                for x in range(0, len(crates)):
-                    if crates[x][nb_from * 2] != " ":
-                        print(crates[x][nb_from * 2])
-                        for y in range(len(crates), 0, -1):
-                            print(y)
-                            if crates[y][nb_to * 2] == " ":
-                                print("move", crates[x][nb_from * 2], " from ", nb_from, " to ", nb_to)
-                                crates[y][nb_to * 2] = crates[x][nb_from * 2]
-                                crates[x][nb_from * 2] = " "
-                                break
-                        break
-                printcrates()
+    setcrates(input)
 
-    printcrates()
+    cleanstacks()
+    for i in input:
+        t = i.split(' ')
+        if t[0] == 'move':
+            nb = int(t[1])
+            fr = int(t[3]) - 1
+            to = int(t[5]) - 1
+            for r in range(0, nb):
+                l = stacks[fr].pop()
+                stacks[to].append(l)
+
+    for i in range(0, len(stacks)):
+        print(stacks[i].pop(), end="")
+
 
 
 def part2(input):
@@ -72,5 +74,5 @@ if __name__ == '__main__':
     # part2(input)
     sample = open("output").read()
     input = sample.split('\n')
-    # part1(input)
+    part1(input)
     # part2(input)
