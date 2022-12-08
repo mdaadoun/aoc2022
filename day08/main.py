85ccaf0a84e30c869d58c19e25075b72077b9455
mdaadoun <daadounmichael@gmail.com>
2022-12-08 14:21:42 +0100

    update

--- day08/main.py
+++ day08/main.py
@@ -17,12 +17,6 @@ def build_grid(input):
 
     max_x = len(grid[0]) - 1
     max_y = len(grid) - 1
-    # print(grid)
-    # print(len(grid))
-    # print(grid[0][0])
-    # print(max_x, max_y)
-    # print(len(grid) - 1, len(grid[0]) - 1)
-    # print(grid[max_y-1][max_x])
 
 
 def build_forest():
@@ -33,17 +27,17 @@ def build_forest():
         for x in range(max_x + 1):
             trees["x:" + str(x) + " y:" + str(y)] = {
                 "height": int(grid[y][x]),
-                "visible": True
+                "visible": True,
+                "view": 0
             }
-
-    # print(trees)
 
 
 def get_tree_data(x, y):
     tree = "x:" + str(x) + " y:" + str(y)
     height = trees[tree]['height']
     visible = trees[tree]['visible']
-    return height, visible
+    view = trees[tree]['view']
+    return height, visible, view
 
 
 def set_tree_not_visible(x, y):
@@ -61,15 +55,12 @@ def check_visibility(x, y):
     }
     visible = False
 
-    # print(x, y, tree[0])
     for k in checks.keys():
         cx = checks[k][0]
         cy = checks[k][1]
-        # print(k, "check")
         while cx != x or cy != y:
             check = get_tree_data(cx, cy)
             if check[0] >= tree[0]:
-                # print("The check hide the tree")
                 break
             if k == "top":
                 cy += 1
@@ -80,30 +71,12 @@ def check_visibility(x, y):
             if k == "right":
                 cx -= 1
         if cx == x and cy == y:
-            # print("the tree is visible")
             visible = True
             break
 
     if not visible:
         set_tree_not_visible(x, y)
-    # print("top", get_tree_data(top_tree[0], top_tree[1]))
-    # print("bottom", get_tree_data(bottom_tree[0], bottom_tree[1]))
-    # print("left", get_tree_data(left_tree[0], left_tree[1]))
-    # print("right", get_tree_data(right_tree[0], right_tree[1]))
 
-
-def solve1():
-    last_tree_x = max_x
-    last_tree_y = max_y
-
-    counter_y = 1
-    while (counter_y < last_tree_y):
-        counter_x = 1
-        while (counter_x < last_tree_x):
-            check_visibility(counter_x, counter_y)
-            counter_x += 1
-            # print()
-        counter_y += 1
 
 def count_visible():
     count = 0
@@ -113,6 +86,74 @@ def count_visible():
             count += 1
     return count
 
+
+def solve1():
+    last_tree_x = max_x
+    last_tree_y = max_y
+
+    cy = 1
+    while cy < last_tree_y:
+        cx = 1
+        while cx < last_tree_x:
+            check_visibility(cx, cy)
+            cx += 1
+        cy += 1
+
+
+def check_scenic_view(x, y):
+    tree = get_tree_data(x, y)
+    views = {
+        "top": 0,
+        "bottom": 0,
+        "left": 0,
+        "right": 0
+    }
+
+    for v in views:
+        cx = x
+        cy = y
+        while True:
+            if v == "top":
+                cy -= 1
+            if v == "bottom":
+                cy += 1
+            if v == "left":
+                cx -= 1
+            if v == "right":
+                cx += 1
+            if cy < 0 or cy > max_y or cx < 0 or cx > max_x:
+                break
+            views[v] += 1
+            check = get_tree_data(cx, cy)
+            if tree[0] <= check[0]:
+                break
+    t = "x:" + str(x) + " y:" + str(y)
+    r = views['top'] * views['bottom'] * views['left'] * views['right']
+    trees[t]['view'] = r
+
+
+def solve2():
+    last_tree_x = max_x + 1
+    last_tree_y = max_y + 1
+
+    cy = 0
+    while cy < last_tree_y:
+        cx = 0
+        while cx < last_tree_x:
+            check_scenic_view(cx, cy)
+            cx += 1
+        cy += 1
+
+
+def best_scenic_view():
+    v = 0
+    for t in trees:
+        c = trees[t]['view']
+        if c > v:
+            v = c
+    return v
+
+
 def part1(input):
     print("part 1:")
     build_grid(input)
@@ -120,16 +161,21 @@ def part1(input):
     solve1()
     print(count_visible())
 
+
 def part2(input):
     print("part 2:")
+    build_grid(input)
+    build_forest()
+    solve2()
+    print(best_scenic_view())
 
 
 if __name__ == '__main__':
     sample = open("sample").read()
     input = sample.split('\n')
     part1(input)
-    # part2(input)
+    part2(input)
     sample = open("input").read()
     input = sample.split('\n')
     part1(input)
-    # part2(input)
+    part2(input)
